a9e056aec952509a4824c2134c1869d1067955d3
Кондусов Александр Александрович <kondusov_aa@ACADEMY.TOP>
2025-11-17 13:27:19 +0300

    добавили сортировку по кол-ву символов

--- algoritms/sort/sort-size-string.js
+++ algoritms/sort/sort-size-string.js
@@ -1,3 +1,23 @@
+function sortByLength(arr){
+    var tmp;
+    for(var i = 0; i < arr.length - 1; i++){
+        if(arr[i + 1].length < arr[i].length){
+            tmp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = tmp;
+        }
+    }
+    return arr;
+}
+
+let arr1 = ["banana", "kivi", "apple"]
+sortByLength(arr1);
+console.log(arr1); 
+
+
+
+//еще одна вариация сортировки (используя количество символов для индекса
+// при добавлении в новый массив)
 function sortAlf(arr){
     let arrResult = {};
     for(var i = 0; i <= arr.length - 1; i++){
